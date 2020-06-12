ea548003aa4a86fa30f7f955b44c000ef70e8db7
coreyl71 <coreyway@163.com>
2020-06-12 19:21:19 +0800

    Initial commit

---
+++ app/src/main/obj/local/arm64-v8a/objs/serial_port/SerialPort.o.d
@@ -0,0 +1,5 @@
+E:/CodeRepo/Demo/ComAssistant1/app/src/main/obj/local/arm64-v8a/objs/serial_port/SerialPort.o: \
+  E:\CodeRepo\Demo\ComAssistant1\app\src\main\jni\SerialPort.c \
+  E:\CodeRepo\Demo\ComAssistant1\app\src\main\jni\SerialPort.h
+
+E:\CodeRepo\Demo\ComAssistant1\app\src\main\jni\SerialPort.h:
---
+++ app/src/main/obj/local/armeabi-v7a/objs/serial_port/SerialPort.o.d
@@ -0,0 +1,5 @@
+E:/CodeRepo/Demo/ComAssistant1/app/src/main/obj/local/armeabi-v7a/objs/serial_port/SerialPort.o: \
+  E:\CodeRepo\Demo\ComAssistant1\app\src\main\jni\SerialPort.c \
+  E:\CodeRepo\Demo\ComAssistant1\app\src\main\jni\SerialPort.h
+
+E:\CodeRepo\Demo\ComAssistant1\app\src\main\jni\SerialPort.h:
